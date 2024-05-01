ac6030f4316c355074bfabb1a998ecb4a34dbe1c
joshuadaquila <138364264+joshuadaquila@users.noreply.github.com>
2024-05-01 08:15:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(session({
   secret: "joshuadaq",
   saveUninitialized: false,
   resave: false,
-  cookie: { secure: false, httpOnly: false, sameSite: "lax" }, // Updated secure to true
+  cookie: { secure: false, httpOnly: false, sameSite: "strict" }, // Updated secure to true
   store: MongoStore.create({
     mongoUrl: "mongodb+srv://joshuadaq:Scribbles24.@cluster0.59wsybx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   })
